5d477c559c9a627ea9c2a465fe884de6aa72c45b
shahidkm <kmshahid432@gmail.com>
2026-06-18 08:18:46 +0530

    loginpagr

--- src/api/queryKeys.ts
+++
@@ -1,5 +0,0 @@
-export const queryKeys = {
-  trainers: {
-    all: ['trainers'] as const,
-  },
-};
--- src/features/trainers/api/trainerQueries.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { createTrainer } from './trainerApi';
-import { TrainerPayload } from '../types/trainer.types';
-
-export const useCreateTrainer = () =>
-  useMutation({
-    mutationFn: (payload: TrainerPayload) => createTrainer(payload),
-  });
